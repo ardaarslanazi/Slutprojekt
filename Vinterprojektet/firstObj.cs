3b53131845680cc1c203a5116b5f4742f3b14c74
arda <ardaburc.arslanazi@elev.ga.ntig.se>
2022-04-04 13:31:43 +0200

    qASD

--- Vinterprojektet/firstObj.cs
+++ Vinterprojektet/firstObj.cs
@@ -5,10 +5,10 @@ using System.Threading;
 
 namespace Vinterprojektet
 {
-    // barn specs allt ifrån position till hastighet
+    // barn specs allt som tar arvet från Ground som är grudnen till alla barn specs
     public class First : Ground
     {
-
+        //bilden
         public Texture2D firstPic = Raylib.LoadTexture("epicbaby.png");
 
         static Random generator = new Random();
@@ -19,7 +19,7 @@ namespace Vinterprojektet
 
 
 
-        //barn specs, bilder och rörelse
+        //barn specs och if sats om att kunna ta bort den
 
         public void SpawnObj()
         {
@@ -35,7 +35,7 @@ namespace Vinterprojektet
 
 
 
-
+            // om musen är på en viss position och klickad invisibility är true
             if (mouseX >= posX && mouseX < posX + 88 && mouseY >= posY && posY < posY + 80 && Raylib.IsMouseButtonPressed(MouseButton.MOUSE_LEFT_BUTTON))
             {
 
